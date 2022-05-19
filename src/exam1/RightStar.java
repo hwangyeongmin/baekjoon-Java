2d6944a98c60c62551b2ac98ca48eb31b99fb221
hwang <syhomi@naver.com>
2022-05-19 14:46:08 +0900

    처음

---
+++ src/exam1/RightStar.java
@@ -0,0 +1,33 @@
+package exam1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class RightStar {
+	  public static void main(String[] args)throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	        
+	        int N = Integer.parseInt(br.readLine());
+	        br.close();
+	        int i,j ;
+	        
+	        for(i=1; i<=N; i++){	        	
+	            for(j=1; j<=N-i; j++){
+	            	bw.write(" ");	            	
+	            }
+	            for(j=N-i+1; j<=N; j++) {
+	            	bw.write("*");
+	            }
+	           	                        
+	            bw.newLine();
+	        }
+	        bw.flush();
+	        bw.close();
+	        
+	    }
+	}
+
