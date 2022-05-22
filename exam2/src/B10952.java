89c04bcb4adabaa86c6c7c2485f344ef4226902b
hwang <syhomi@naver.com>
2022-05-22 14:23:24 +0900

    2022.05.22

--- exam2/src/B10952.java
+++ exam2/src/B10952.java
@@ -1,9 +1,34 @@
 
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class B10952 {
 	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st;
+		
+		int A, B;
+		while(true) {
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			
+			if(A==0 & B==0) {
+				break;
+			}
+			bw.write((A+B)+"\n");
+		}
+		
+		bw.flush();
+		bw.close();
 		
 	}
 }
